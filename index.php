5847db9e8395fbeac7b9d08da024026e76655fa2
Kroepniek <mondzielewski123@wp.pl>
2019-01-15 15:30:37 +0100

    Init commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Vault-Js">
+	<link rel="icon" href="images/transparent_kon.png">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/digit.js"></script>
+	<title>Daniel Mondzielewski</title>
+</head>
+<body>
+	<div id="container">
+	</div>
+</body>
+</html>
